aebd1d964f7f64e6aab26e5859a346856748cc2c
choitree <choitree@218.38.137.27>
2020-04-27 18:51:10 +0900

    reviewUpdatecontroller

--- src/notebook/controller/review/ReviewUpdateController.java
+++ src/notebook/controller/review/ReviewUpdateController.java
@@ -1,5 +1,8 @@
 package notebook.controller.review;
 
+import java.io.File;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import notebook.controller.Controller;
 import notebook.controller.ModelAndView;
 import notebook.domain.BoardReview;
+import notebook.exception.CannotModifyException;
 import notebook.exception.NotEnoughParameterException;
 import notebook.service.ReviewService;
 
@@ -14,7 +18,7 @@ public class ReviewUpdateController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+		ModelAndView mv = null;
 		String content = request.getParameter("content");
 		String useId = request.getParameter("userId");
 		String reviewNo = request.getParameter("reviewNo");
@@ -30,11 +34,34 @@ public class ReviewUpdateController implements Controller {
 			throw new NotEnoughParameterException("입력값이 충분하지 않습니다.");
 		}
 		
-		BoardReview review = new BoardReview(Integer.parseInt(reviewNo), userId, imgName, content, serialNum, Integer.parseInt(grade) );
 		
-		ReviewService.update(review);
-		ModelAndView mv = new ModelAndView(true, "reviewSelectBySerialNum page");
+		try {
+			boolean bFind = false;
+			
+			List<BoardReview> ls = ReviewService.searchByUserId(userId);
+			for(BoardReview br : ls) {
+				if(br.getReviewNo() == Integer.parseInt(reviewNo)) {
+					bFind = true;
+					BoardReview review = new BoardReview(Integer.parseInt(reviewNo), userId, imgName, content, serialNum, Integer.parseInt(grade) );
+					ReviewService.update(review);
+					 mv = new ModelAndView(true, "reviewSelectBySerialNum page");
+					break;
+				}
+				
+				if(!bFind) {
+					throw new CannotModifyException("수정하려는 리뷰는 다른 작성자의 리뷰 입니다.");
+				}
+				
+			}
+			
+		}catch(Exception e) {
+			request.setAttribute("errorMsg", e.getMessage());
+			String url = "errorView/error.jsp";
+		}
+		
+		
 		return mv;
+		
 	
 	}
 
